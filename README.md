85a1d5245c2c25e4632ff0e3981c1f2f354b0bde
kyrstinlawson <kyrstin.lawson@gmail.com>
2022-05-29 11:40:10 -0700

    Updated CSS styles

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # techdegree-project-4
 OOP Game Show App
+
+CSS Changes: 
+(1) Made buttons round 
+(2) Correctly chosen letters turn green
+(3) Wrong letters disappear from onscreen keyboard
--- js/app.js
+++ js/app.js
@@ -7,11 +7,13 @@ const startButton = document.getElementById("btn__reset");
 const keyboard = document.getElementById("qwerty");
 const keys = document.querySelectorAll(".key");
 
+// Starts new game when start button is clicked
 startButton.addEventListener("click", () => {
     game = new Game();
     game.startGame();
 });
 
+// When keys on screen on clicked with mouse it triggers the handleInteraction function
 keyboard.onclick = (e) => {
     let target = e.target;
     if (target.className === "key") {
@@ -19,7 +21,11 @@ keyboard.onclick = (e) => {
     };
 };
 
-// document.addEventListener("keyup", (e) => {
-//     console.log(e.key);
-//     for (let i=0; i < ) {}
-// });
+// When keys on keyboard are used it triggers the handleInteraction function
+document.addEventListener("keyup", (e) => {
+    for (let i=0; i < keys.length; i++) {
+        if (e.key === keys[i].innerHTML && keys[i].disabled === false) {
+            game.handleInteraction(keys[i]);
+        }
+    }
+});
